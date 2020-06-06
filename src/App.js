9efbf83b5ab73066d170c51a2cad73206b05553f
Mohammad Hammad Khan <66178178+mhkdesigns@users.noreply.github.com>
2020-06-06 19:59:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         <br>
-        Update by MHK
+        Update by MHK designs
         </br>
         </p>
         <a
